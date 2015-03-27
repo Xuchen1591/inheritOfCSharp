35f051c136b7ee8837f91e2bdf2cbd7185e8d3c8
magina.1591@outlook.com <magina.1591@outlook.com>
2015-03-27 21:12:18 +0800

    use key word:'new' to declare override

---
+++ InheritOfCSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InheritOfCSharp
+{
+    class myBaseClass
+    {
+        public virtual void virtualMethod()
+        {
+            Console.WriteLine("this is base menthod");
+        }
+
+
+        public void hidableMethod()
+        {
+            Console.WriteLine("this is base hidable method");
+        }
+    }
+
+    class myDerivedClass : myBaseClass
+    {
+        public override void virtualMethod()
+        {
+            //if not override, default use base class's method
+            base.virtualMethod();
+
+            //if yes, rewrite and override baseclass function
+            //Console.WriteLine("this is base menthod+ derived class");
+        }
+
+        //if the son-class has the same function(wi same name) with base class
+        //we have to use key word:'new' to declare
+        public new void hidableMethod()
+        {
+            Console.WriteLine("this is son hidable method");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myBaseClass bc = new myBaseClass();
+            myDerivedClass dc = new myDerivedClass();
+            bc.virtualMethod();
+            dc.virtualMethod();
+
+            bc.hidableMethod();
+            dc.hidableMethod();
+        }
+    }
+}
